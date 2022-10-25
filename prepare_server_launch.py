da5897bba710227a586ab19b256cb9bafd20c734
Canaan <reakak1@gmail.com>
2022-10-25 14:41:18 +0300

    now opens the apps

--- prepare_server_launch.py
+++ prepare_server_launch.py
@@ -4,24 +4,34 @@
     TODO: Maybe make it pretty ??
 """
 
+from time import sleep
 from typing import List
-
+import threading
 # make these a cli argument
 # like python <scriptname> -r [what to remove] -db to truncate database tables 
 dirs_to_remove : List[str] = [
-    r"D:\development\af-framework\ArabyAds.Framework.DistributedEventBroker\ArabyAds.Framework.DistributedEventBroker.Core\bin\Debug\net6.0\kafka.offline",
-    r"D:\development\dsp-platform\src-netcore\ArabyAds.AdFalcon.Banker\ArabyAds.AdFalcon.Banker.Master\bin\Debug\net6.0\kafka.offline",
-    r"D:\development\dsp-platform\src-netcore\ArabyAds.AdFalcon.Server.BiddingPrediction\ArabyAds.AdFalcon.Server.BiddingPrediction.Service.Host\bin\Debug\net6.0\kafka.offline",
-    r"D:\development\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.MaintenanceJob\bin\Debug\net6.0\kafka.offline",
-    r"D:\development\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.WebAPI\bin\Debug\net6.0\kafka.offline",
-    r"G:\af-framework\ArabyAds.Framework.DistributedEventBroker\ArabyAds.Framework.DistributedEventBroker.Core\bin\Debug\net6.0\kafka.offline",
-    r"G:\dsp-platform\src-netcore\ArabyAds.AdFalcon.Banker\ArabyAds.AdFalcon.Banker.Master\bin\Debug\net6.0\kafka.offline",
-    r"G:\dsp-platform\src-netcore\ArabyAds.AdFalcon.Server.BiddingPrediction\ArabyAds.AdFalcon.Server.BiddingPrediction.Service.Host\bin\Debug\net6.0\kafka.offline",
-    r"G:\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.MaintenanceJob\bin\Debug\net6.0\kafka.offline",
-    r"G:\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.WebAPI\bin\Debug\net6.0\kafka.offline"
-
-
+    # r"D:\development\af-framework\ArabyAds.Framework.DistributedEventBroker\ArabyAds.Framework.DistributedEventBroker.Core\bin\Debug\net6.0\kafka.offline",
+    # r"D:\development\dsp-platform\src-netcore\ArabyAds.AdFalcon.Banker\ArabyAds.AdFalcon.Banker.Master\bin\Debug\net6.0\kafka.offline",
+    # r"D:\development\dsp-platform\src-netcore\ArabyAds.AdFalcon.Server.BiddingPrediction\ArabyAds.AdFalcon.Server.BiddingPrediction.Service.Host\bin\Debug\net6.0\kafka.offline",
+    # r"D:\development\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.MaintenanceJob\bin\Debug\net6.0\kafka.offline",
+    # r"D:\development\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.WebAPI\bin\Debug\net6.0\kafka.offline",
+   
+    r"E:\development\arabyads_work\af-framework\ArabyAds.Framework.DistributedEventBroker\ArabyAds.Framework.DistributedEventBroker.Core\bin\Debug\net6.0\kafka.offline",
+    r"E:\development\arabyads_work\dsp-platform\src-netcore\ArabyAds.AdFalcon.Banker\ArabyAds.AdFalcon.Banker.Master\bin\Debug\net6.0\kafka.offline",
+    r"E:\development\arabyads_work\dsp-platform\src-netcore\ArabyAds.AdFalcon.Server.BiddingPrediction\ArabyAds.AdFalcon.Server.BiddingPrediction.Service.Host\bin\Debug\net6.0\kafka.offline",
+    r"E:\development\arabyads_work\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.MaintenanceJob\bin\Debug\net6.0\kafka.offline",
+    r"E:\development\arabyads_work\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.WebAPI\bin\Debug\net6.0\kafka.offline",
 ]
+programs_to_run : List[str] = [
+
+    r"E:\development\arabyads_Work\af-framework\ArabyAds.Framework.DistributedEventBroker\ArabyAds.Framework.DistributedEventBroker.Core\bin\Debug\net6.0\ArabyAds.Framework.DistributedEventBroker.Core.exe",
+    r"E:\development\arabyads_Work\dsp-platform\src-netcore\ArabyAds.AdFalcon.Banker\ArabyAds.AdFalcon.Banker.Master\bin\Debug\net6.0\ArabyAds.AdFalcon.Banker.Master.exe",
+    r"E:\development\arabyads_Work\dsp-platform\src-netcore\ArabyAds.AdFalcon.Server.BiddingPrediction\ArabyAds.AdFalcon.Server.BiddingPrediction.Service.Host\bin\Debug\net6.0\ArabyAds.AdFalcon.Server.BiddingPrediction.Service.Host.exe",
+    r"E:\development\arabyads_Work\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.MaintenanceJob\bin\Debug\net6.0\ArabyAds.AdFalcon.Server.MaintenanceJob.exe",
+    r"E:\development\arabyads_Work\dsp-platform\src-netcore\AdFalcon.AdServer\ArabyAds.AdFalcon.Server.WebAPI\bin\Debug\net6.0\ArabyAds.AdFalcon.Server.WebAPI.exe",
+]
+
+
 
 tables_to_truncate : List[str] = [
     "bc_billing_entity_spend_details",
@@ -49,9 +59,10 @@ def remove_offline_db() -> None :
             print(f"Couldn't remove cause: \n{type(ex).__name__}\n{folder}")
             continue
 
+
 def truncate_tables() -> None:
     """Empties the tables relied upon for the service bus?? anyways they aren't needed"""
-    from sqlalchemy import create_engine, text
+    from sqlalchemy import create_engine
     engine = create_engine("mysql+pymysql://root:root@localhost/adfalcon?charset=utf8")
     print(f"\n{seperator *15}\n")
     with engine.connect() as conn:
@@ -65,12 +76,31 @@ def truncate_tables() -> None:
                print(f"something died -> something: {ex}")
                #* "anyways"
                continue
+    print(f"\n{seperator *15}\n")
+    
+
+def run_apps()-> None:
+    """Runs the apps defined in the apps list"""
+
+    import subprocess
+    for program in programs_to_run:
+        app_name= program.split('\\')[-1]
+        print(f"opening:\t{app_name}")
+        sleep(1)
+        subprocess.Popen(program,creationflags=subprocess.CREATE_NEW_CONSOLE )
+
+def main():
+    jobs : List[threading.Thread] = [
+        threading.Thread(target=remove_offline_db),
+        threading.Thread(target=truncate_tables),
+        threading.Thread(target=run_apps)
+    ]
+    for job in jobs:  job.start()
+
 
 
 if __name__ == "__main__":
-    import threading
-    jobs : List[threading.Thread] = [
-        threading.Thread(target=remove_offline_db),
-        threading.Thread(target=truncate_tables)
-    ]
-    for job in jobs: job.start()
+    main()
+
+
+    
